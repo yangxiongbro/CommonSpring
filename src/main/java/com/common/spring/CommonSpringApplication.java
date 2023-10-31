8521e8a9d49dc6fffd5906f1b8cb1b41c8cf9c9a
yangxiongbro <yangxiongbro@163.com>
2023-11-01 00:05:45 +0800

    init:初始化项目

---
+++ src/main/java/com/common/spring/CommonSpringApplication.java
@@ -0,0 +1,14 @@
+package com.common.spring;
+
+/**
+ * <b><code>CommonSpringApplication</code></b>
+ * <p/>
+ * <p>
+ * <p/>
+ * <b>Creation Time:</b> 2023/11/1 0:03
+ *
+ * @author yang xiong
+ * @since CommonSpring 1.0
+ */
+public class CommonSpringApplication {
+}
---
+++ src/test/java/com/common/spring/CommonSpringApplicationTest.java
@@ -0,0 +1,14 @@
+package com.common.spring;
+
+/**
+ * <b><code>CommonSpringApplicationTest</code></b>
+ * <p/>
+ * <p>
+ * <p/>
+ * <b>Creation Time:</b> 2023/11/1 0:04
+ *
+ * @author yang xiong
+ * @since CommonSpring 1.0
+ */
+public class CommonSpringApplicationTest {
+}
